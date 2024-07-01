e36dbe194261368f2fc26cc305527d49bb8619a8
AlexDDks <magarciaa92@gmail.com>
2024-07-01 17:56:48 -0600

    probando vercel 2

--- app.js
+++ app.js
@@ -1,69 +1,70 @@
-const express = require("express"); // We require from Node the "express" framework 
-const app = express(); // We invoke the variable express and save the funcionality of it in the const app
+const express = require("express"); // Requerimos el framework "express" de Node 
+const app = express(); // Invocamos express y guardamos su funcionalidad en la constante app
+const dotenv = require("dotenv"); // Requerimos dotenv para manejar variables de entorno
+dotenv.config(); // Configuramos dotenv para cargar las variables de entorno
 
-const path = require("path"); // We require from Node the native module path
-const publicPath = path.join(__dirname, "public"); // We save the funcionality of the method resolve in publicPath in order to have a public folder. We just have created an absolute path which will be used for the method static of express. It recieves the parameters of the route. We could just put in the arguments that the folder is PUBLIC, but in another PC, the absolute route could be different, for that reason we use the string DIRNAME, so we can reach the public folder in an absolute enviroment. __dirname is a variable that give to us the name of the folder where we are, so with __dirname, "public", we create a path that ends in the folder "public".
-const globalConstants = require("./const/globalConstants") //We require the module globalConstants which saves information to conect with the DB
-const db = require("./database/models") //We require the module globalConstants which saves information to conect with the DB
+const path = require("path"); // Requerimos el módulo nativo path
+const publicPath = path.join(__dirname, "public"); // Creamos una ruta absoluta hacia la carpeta public
 
+const globalConstants = require("./const/globalConstants"); // Requerimos el módulo globalConstants que guarda información para conectar con la DB
+const db = require("./database/models"); // Requerimos los modelos de la base de datos
 
-const session = require("express-session")//We require this module and save all its functionallity in this constant. The sessions variables are globals and I am using those in the views, where I need to share information of a logged user, for example.
-app.use(session({secret: "Secret!!!"})) //Using of session at app level (based on documentation)
+const session = require("express-session"); // Requerimos express-session y guardamos su funcionalidad
+app.use(session({
+    secret: "Secret!!!",
+    resave: false,
+    saveUninitialized: true
+})); // Configuramos la sesión en el nivel de la aplicación
 
-//Public folder
-app.use(express.static(publicPath)) // Using the method static we tell to express where the direction of public folder is. It recieves a root that in this case is insede the variable publicPath.
+// Configuración de la carpeta public
+app.use(express.static(publicPath)); // Indicamos a express la dirección de la carpeta public
 
 // Cookies
-const cookieParser = require('cookie-parser')//We require this module and save all its functionallity in this constant
-app.use(cookieParser()); // This is a native module, that allows us to use cookies.
+const cookieParser = require('cookie-parser'); // Requerimos cookie-parser
+app.use(cookieParser()); // Habilitamos el uso de cookies
 
-const rememberMiddleware = require("./middlewares/rememberMiddleware")
-app.use(rememberMiddleware) //The middleware of cookie is gonna be executed every time, in every req,res.
+const rememberMiddleware = require("./middlewares/rememberMiddleware");
+app.use(rememberMiddleware); // Ejecutamos el middleware de cookies en cada req, res
 
-//User
-const localsUser = require("./middlewares/localsUser")
-app.use(localsUser)//The middleware of user is gonna be executed every time, in every req,res.
+const localsUser = require("./middlewares/localsUser");
+app.use(localsUser); // Ejecutamos el middleware de usuario en cada req, res
 
-// View engine
-app.set('view engine', 'ejs'); // We are using the EJS view engine, so we tell it to Express in order to use it.
+// Motor de vistas
+app.set('view engine', 'ejs'); // Configuramos el motor de vistas EJS
 
-// POST obligated lines
-app.use(express.urlencoded({ extended: false })) //To capture the information from forms (post), that receives an object with propertie and value extended: false.
-app.use(express.json())
+// Configuración para capturar información de formularios (POST)
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-//PUT-DELETE obligated lines
-const methodOverride = require('method-override')
-app.use(methodOverride('_method'))
+// Configuración para métodos PUT y DELETE
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
-// Routes
-const mainRouter = require("./routes/mainRouter")
-const usersRouter = require("./routes/usersRouter")
-const productsRouter = require("./routes/productsRouter")
+// Rutas
+const mainRouter = require("./routes/mainRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
 const cartRouter = require("./routes/cartRouter");
 
-// const servicesRouter = require("./routes/servicesRouter")
-
-// Paths for router
-app.use("/", mainRouter)
-app.use("/users", usersRouter)
-app.use("/products", productsRouter)
+// Configuración de rutas en la aplicación
+app.use("/", mainRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
 app.use("/cart", cartRouter);
 
-// app.use("/services", servicesRouter)
-
-// // 404 error set
+// // Configuración de error 404
 // app.use((req, res, next) => {
-//     res.status(404).render('not-found')
-// })
+//     res.status(404).render('not-found');
+// });
 
-//Setting
-const PORT = globalConstants.PORT
+// Configuración del puerto
+const PORT = process.env.PORT || 3000;
 
-// Server and promise
-db.sequelize.sync().then(() => {//This method sincronize the models in Sequelize with the DB
-    app.listen(PORT, () => { // Whit the listen method, we created a server in the port 3000. In order to figured out if the server is working, we can add a message in console that is gonna be shown only if rhe server is running
-        return console.log(`Server has been created in port ${PORT}`)
-    })
+// Iniciar el servidor
+db.sequelize.sync().then(() => { // Sincronizamos los modelos en Sequelize con la DB
+    app.listen(PORT, () => { // Creamos un servidor en el puerto especificado
+        return console.log(`Server has been created in port ${PORT}`);
+    });
 }).catch((error) => {
     console.error('Error during database synchronization or server startup', error);
 });
